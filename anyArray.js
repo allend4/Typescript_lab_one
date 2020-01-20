dd8d7845a8689e5c10a949c18e31dd89eb38ba09
DAVID ALLEN <G00375372@gmit.ie>
2020-01-20 17:54:07 +0000

    AnyArray Example Updated

--- anyArray.js
+++ anyArray.js
@@ -2,3 +2,7 @@ var listany = [1, true, "free"];
 for (var i = 0; i < listany.length; i++) {
     console.log("Before: Entry " + (i + 1) + " is: " + listany[i] + ".");
 }
+listany[1] = 100;
+for (var i = 0; i < listany.length; i++) {
+    console.log("After: Entry " + (i + 1) + " is: " + listany[i] + ".");
+}
--- anyArray.ts
+++ anyArray.ts
@@ -3,3 +3,9 @@ let listany: any[] = [1, true, "free"];
 for(let i = 0; i < listany.length;i++){ 
     console.log("Before: Entry "+(i+1)+" is: "+listany[i]+"."); 
 }
+
+
+listany[1] = 100; 
+for(let i = 0; i < listany.length;i++){ 
+    console.log("After: Entry "+(i+1)+" is: "+listany[i]+".")
+}
