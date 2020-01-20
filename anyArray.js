cccbd91a632dd2980def6b296e2582d2e9a85c06
DAVID ALLEN <G00375372@gmit.ie>
2020-01-20 17:49:48 +0000

    AnyArray Example

---
+++ anyArray.js
@@ -0,0 +1,4 @@
+var listany = [1, true, "free"];
+for (var i = 0; i < listany.length; i++) {
+    console.log("Before: Entry " + (i + 1) + " is: " + listany[i] + ".");
+}
---
+++ anyArray.ts
@@ -0,0 +1,5 @@
+let listany: any[] = [1, true, "free"]; 
+
+for(let i = 0; i < listany.length;i++){ 
+    console.log("Before: Entry "+(i+1)+" is: "+listany[i]+"."); 
+}
