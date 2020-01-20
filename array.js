3792acef8cf68f7547931c7d9bf503d3a1bccc8a
DAVID ALLEN <G00375372@gmit.ie>
2020-01-20 17:22:22 +0000

    Array Example

---
+++ array.js
@@ -0,0 +1,4 @@
+var list = [1, 2, 3];
+console.log(list);
+//another version
+//let list: number[] = [1, 2, 3];
---
+++ array.ts
@@ -0,0 +1,5 @@
+let list: Array<number> = [1, 2, 3];
+console.log(list);
+
+//another version
+//let list: number[] = [1, 2, 3];
